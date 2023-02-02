2daa843af47df9a405ac6c4d884602d400d0499c
nesuesu <viktor.lavrinovic@gmail.com>
2023-02-02 14:53:13 +0200

    Add form with POST to the server added

--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -8,6 +8,14 @@ const PostProvider = ({children}) => {
 
     const [posts, setPosts] = useState(null);
 
+    const postData = (newPost) => {
+        fetch('http://localhost:5000/AnimalsTypes', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newPost),
+        })
+        .then((res) => res.json());
+    }
 
     // ant pirmo užkrovimo atsisiunčiam visus vartotojus iš json serverio ir įsirašom juos į state kintamąjį
     const getPosts = async () => {
@@ -25,6 +33,7 @@ const PostProvider = ({children}) => {
         value = {{
             posts,
             setPosts,
+            postData,
         }}
         >
         {children}
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,8 +1,40 @@
 
+import PostContext from "../contexts/PostContext";
+import { useContext } from "react";
+
 const Add = () => {
+
+    const { postData } = useContext(PostContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Title: `,e.target.title.value);
+        console.log(`Description: `,e.target.description.value);
+        
+        postData({
+            title: e.target.title.value,
+            description: e.target.description.value,
+        });
+
+
+
+    }
+
     return (
         <>
         <h1>Add</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="">Title:
+                <input type="text" name="title" />
+            </label>
+            <br />
+            <label htmlFor="">Description:
+                <textarea name="description" id="desc" cols="30" rows="10"></textarea>
+            </label>
+            <br />
+            <input type="submit" />
+        </form>
+        <hr />
         </>
     )
 }
